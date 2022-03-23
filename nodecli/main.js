f8d6f11bc982acb0de485086379c03bf5dba29dc
h6k2 <sepak.hirokazu10@gmail.com>
2022-03-23 18:00:47 +0900

    package install

--- nodecli/main.js
+++ nodecli/main.js
@@ -1,20 +1,29 @@
-const myModule = require("./my-module");
 const program = require("commander");
 const fs = require("fs");
+const marked = require("marked");
 
-// console.log(myModule.foo);
-
-// console.log('Hello World!');
-
-
+// gfmオプションを定義する
+program.option("--gfm", "GFMを有効にする");
 program.parse(process.argv);
 const filePath = program.args[0];
 
-fs.readFile(filePath, { encoding: "utf8" }, (error, file)=> {
-    if(error) {
-        console.error(error.message);
+// コマンドライン引数のオプションを取得する
+const options = program.opts();
+
+// コマンドライン引数で指定されなかったオプションにデフォルト値を上書きする
+const cliOptions = {
+    gfm: options.gfm ?? false,
+};
+
+fs.readFile(filePath, { encoding: "utf8" }, (err, file) => {
+    if (err) {
+        console.error(err.message);
         process.exit(1);
         return;
-    } 
-    console.log(file);
-})
+    }
+    const html = marked.parse(file, {
+        // オプションの値を使用する
+        gfm: cliOptions.gfm,
+    });
+    console.log(html);
+});
